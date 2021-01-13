559b4a76a3393f601fe2ccd894cbf57ae26f65aa
firdausthedev <firdausthedev@gmail.com>
2021-01-13 19:19:05 +0800

    manage booking

--- resources/views/booking/custViewDrivers.blade.php
+++ resources/views/booking/custViewDrivers.blade.php
@@ -35,10 +35,10 @@
 
   <section id="cust-driver-list" class="container">
     <h2 class="py-4">List of Available Drivers</h2>
-    
+    @foreach ($data as $d)
     <form action="" method="POST" >
     @csrf
-    @foreach ($data as $d)
+    
 
       <div class="driver-list py-3">
         
@@ -68,8 +68,9 @@
         <input type="submit" value="Book" class="driver-book-btn" />
       
       </div>
-    @endforeach
+    
     </form>
+    @endforeach
   </section>
   <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
   <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
